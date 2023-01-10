8369ef14dd3811590b7b012bbacc993d4eb4d191
dinhyenhp01 <122248982+dinhyenhp01@users.noreply.github.com>
2023-01-10 23:27:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,14 +24,3 @@ Amount_to_Customer = st.number_input("Amount to Customer: ")
 Master_Category = st.slider("Master Category ", 1, 12, 1)
 Rank = st.slider("Rank", 1, 52, 4)
 
-# load model
-rf_model = RandomForestRegressor(n_estimators=28,random_state=0)
-rf_model = load_model("rf_regressor.json")
-
-if st.button('Price Prediction'):
-    input_species = encoder.transform(np.expand_dims(inp_species, -1))
-    inputs = np.expand_dims(
-        [int(input_species), Category_Name_ID, Qty, Cash_Discount, Amount_to_Customer, Master_Category, Rank], 0)
-    prediction = rf_model.predict(inputs)
-    print("final pred", np.squeeze(prediction, -1))
-    st.write(f"Price should be: {np.squeeze(prediction, -1):.2f}g")
