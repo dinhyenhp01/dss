4badcde3fb0045cccabad334d1fe27e011a6b6d5
dinhyenhp01 <122248982+dinhyenhp01@users.noreply.github.com>
2023-01-10 16:45:45 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,47 +1,42 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-from sklearn.ensemble import RandomForestRegressor
-from pickle import load
-from keras.models import load_model
+import yfinance as finance
 
-#Loading up the Random Forest Regression Model
-rf_model = RandomForestRegressor(n_estimators=28,random_state=0)
-rf_model = load_model('rf_regressor.py')
 
-def predict(Category_Name, Qty, Cash_Discount, Amount_to_Customer, Master_to_Category, Rank):
-	X_test = pd.DataFrame([[Category_Name, Qty, Cash_Discount, Amount_to_Customer, Master_to_Category, Rank]],
-	columns = ['Category Name', 'Qty', 'Cash Discount', 'Amount to Customer', 'Master to Category', 'Rank'])
-	X_new_test = poly.transform(X_test)
-	X_new_test = pd.DataFrame(data = X_new_test, columns = poly.get_geature_names_out(X_test.columns))
-	X_selector_test = selector.transform(X_new_test)
-	X_test_res = scale.transform(X_selector_test)
-	prediction = model.predict(X_test_res)
-	return predict
+def get_ticker(name):
+	company = finance.Ticker(name) # google
+	return company
 
-st.set_page_config(page_title = "Cosmetic Sales Products Home Page")
-st.title("Cosmetic Sales Products Home Page")
-st.subheader('Predict Price Cosmetic Product Sales')
-st.header('Fill information:')
 
-Category_Name_ID = st.slider("Category Name ID", 0, 8,1)
-Qty = st.number_input("Quantity of Products: ")
-Cash_Discount = st.slider("Cash Discount", 0, 100,10)
-Amount_to_Customer = st.number_input("Amount to Customer: ")
-Master_Category = st.slider("Master Category ", 1, 12, 1)
-Rank = st.slider("Rank", 1, 52, 4)
+# Project Details
+st.title("Build and Deploy Stock Market App Using Streamlit")
+st.header("A Basic Data Science Web Application")
+st.sidebar.header("Geeksforgeeks \n TrueGeeks")
 
-if st.button ('Predict Price'):
-	price = predict(Category_Name, Qty, Cash_Discount, Amount_to_Customer, Master_to_Category, Rank)
-	st.success(f'Predict Price of Cosmetic Product is: {Price[0]}')
+company1 = get_ticker("GOOGL")
+company2 = get_ticker("MSFT")
 
-# ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# fetches the data: Open, Close, High, Low and Volume
+google = finance.download("GOOGL", start="2021-10-01", end="2021-10-01")
+microsoft = finance.download("MSFT", start="2021-10-01", end="2021-10-01")
+
+# Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+data1 = company1.history(period="3mo")
+data2 = company2.history(period="3mo")
+
+# markdown syntax
+st.write("""
+### Google
+""")
+
+# detailed summary on Google
+st.write(company1.info['longBusinessSummary'])
+st.write(google)
+
+# plots the graph
+st.line_chart(data1.values)
+
+st.write("""
+### Microsoft
+""")
+st.write(company2.info['longBusinessSummary'], "\n", microsoft)
+st.line_chart(data2.values)
