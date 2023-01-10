93ec8cabd20d56559b5cd8527a7175bc633b0db2
dinhyenhp01 <122248982+dinhyenhp01@users.noreply.github.com>
2023-01-10 13:40:05 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
-from sklearn.ensemble import RandomForestRegressor
 import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pylot as plt
+from sklearn.ensemble import RandomForestRegressor
 from pickle import load
 
 #Loading up the Random Forest Regression Model we created
