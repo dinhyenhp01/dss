4e5bbe1eba2aa6ba6fe2c61867abf3d0aaecf55b
dinhyenhp01 <122248982+dinhyenhp01@users.noreply.github.com>
2023-01-10 13:55:48 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,23 +1,45 @@
 import streamlit as st
-from predict_cost import predict
+import pandas as pd
 import numpy as np
- 
-st.title('Home price prediction')
- 
-st.write('---')
- 
-# area of the house
-area = st.slider('Area of the house', 1000, 5000, 1500)
- 
-# no. of bedrooms in the house
-bedrooms = st.number_input('No. of bedrooms', min_value=0, step=1)
- 
-# no. of balconies in the house
-balconies = st.radio('No. of balconies', (0, 1, 2 , 3))
- 
-# how old is the house? (age)
-age = st.number_input('How old is the house (in years)?', min_value=0, step=1)
- 
-if st.button('Predict House Price'):
-    cost = predict(np.array([[area, bedrooms, balconies, age]]))
-    st.text(cost[0])
+from sklearn.ensemble import RandomForestRegressor
+from pickle import load
+
+#Loading up the Random Forest Regression Model we created
+model = RandomForestRegressor(n_estimators=28,random_state=0)
+model.load_model('rf_regressor.json')
+
+def predict(Category_Name, Qty, Cash_Discount, Amount_to_Customer, Master_to_Category, Rank):
+	X_test = pd.DataFrame([[Category_Name, Qty, Cash_Discount, Amount_to_Customer, Master_to_Category, Rank]],
+	columns = ['Category Name', 'Qty', 'Cash Discount', 'Amount to Customer', 'Master to Category', 'Rank'])
+	X_new_test = poly.transform(X_test)
+	X_new_test = pd.DataFrame(data = X_new_test, columns = poly.get_geature_names_out(X_test.columns))
+	X_selector_test = selector.transform(X_new_test)
+	X_test_res = scale.transform(X_selector_test)
+	prediction = model.predict(X_test_res)
+	return predict
+
+st.title("Cosmetic Sales Products Home Page")
+st.subheader('Predict Price Cosmetic Product Sales')
+st.header('Fill information:')
+
+Category_Name_ID = st.slider("Category Name ID", 0, 8,1)
+Qty = st.number_input("Quantity of Products: ")
+Cash_Discount = st.slider("Cash Discount", 0, 100,10)
+Amount_to_Customer = st.number_input("Amount to Customer: ")
+Master_Category = st.slider("Master Category ", 1, 12, 1)
+Rank = st.slider("Rank", 1, 52, 4)
+
+def Price(value):
+	if st.button ('Predict Price'):
+		price = predict(Category_Name, Qty, Cash_Discount, Amount_to_Customer, Master_to_Category, Rank)
+		st.success(f'Predict Price of Cosmetic Product is: {Price[0]}')
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
