e78b8d9c60ba48b3ba45c60a32214f93d5e06d2e
dinhyenhp01 <122248982+dinhyenhp01@users.noreply.github.com>
2023-01-10 12:55:35 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pickle import load
 
 #Loading up the Random Forest Regression Model we created
-model = RandomForestRegressor(random_state = 0)
+model = RandomForestRegressor(n_estimators=28,random_state=0)
 model.load_model('EDA/rf_regressor.json')
 
 def predict(Category_Name, Qty, Cash_Discount, Amount_to_Customer, Master_to_Category, Rank):
