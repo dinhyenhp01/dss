14738ee11ff3839d77d598a5a5d99bfc201efbdb
dinhyenhp01 <122248982+dinhyenhp01@users.noreply.github.com>
2023-01-11 11:22:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import h5py
 
 st.header("Cosmetic Price Sales Product Prediction")
 
-data = pd.read_csv("https://raw.githubusercontent.com/dinhyenhp01/dss/main/Cosmetic_products_sales_clean.csv?token=GHSAT0AAAAAAB5HGBWOIXYZ4LPQOJ4VL6PMY56F5LQ")
+data = pd.read_csv("https://raw.githubusercontent.com/dinhyenhp01/dss/main/Cosmetic_products_sales_clean.csv")
 
 if st.checkbox('Our data'):
     data
